80a2e9fc2151e0a96ec26472fb4c4a26610c63a9
David Lugato <david.lugato@gmail.com>
2017-07-11 14:15:25 -0400

    Adding plus3 to GMM

--- gmm4taco.h
+++ gmm4taco.h
@@ -10,11 +10,22 @@ typedef gmm::csc_matrix<double> GmmSparse;
 typedef gmm::col_matrix< gmm::wsvector<double> > GmmDynSparse;
 #endif
 
-  void tacoToGMM(const Tensor<double>& src, GmmSparse& dst) {
-    GmmDynSparse tmp(src.getDimension(0), src.getDimension(1));
+  void GMMTotaco(const GmmDynSparse& src, Tensor<double>& dst) {
+    for (int j = 0; j < gmm::mat_ncols(src); ++j) {
+      typename gmm::linalg_traits<GmmDynSparse>::const_sub_col_type col = mat_const_col(src, j);
+      typename gmm::linalg_traits<gmm::wsvector<double>>::const_iterator it1 = vect_const_begin(col);
+      typename gmm::linalg_traits<gmm::wsvector<double>>::const_iterator ite1 = vect_const_end(col);
+      while (it1 != ite1) {
+        dst.insert({(int)(it1.index()),j},*it1);
+        ++it1;
+      }
+    }
+    dst.pack();
+  }
+
+  void tacoToGMM(const Tensor<double>& src, GmmDynSparse& dst) {
     for (auto& value : iterate<double>(src))
-      tmp(value.first.at(0),value.first.at(1)) = value.second;
-    gmm::copy(tmp, dst);
+      dst(value.first.at(0),value.first.at(1)) = value.second;
   }
 
   void GMMTotaco(const std::vector<double>& src, Tensor<double>& dst){
@@ -34,8 +45,10 @@ typedef gmm::col_matrix< gmm::wsvector<double> > GmmDynSparse;
         int rows=exprOperands.at("A").getDimension(0);
         int cols=exprOperands.at("A").getDimension(1);
 
+        GmmDynSparse Agmm_tmp(rows,cols);
+        tacoToGMM(exprOperands.at("A"),Agmm_tmp);
         GmmSparse Agmm(rows,cols);
-        tacoToGMM(exprOperands.at("A"),Agmm);
+        gmm::copy(Agmm_tmp, Agmm);
         std::vector<double> xgmm(cols), ygmm(rows);
         tacoToGMM(exprOperands.at("x"),xgmm);
 
@@ -45,6 +58,26 @@ typedef gmm::col_matrix< gmm::wsvector<double> > GmmDynSparse;
         GMMTotaco(ygmm,y_gmm);
 
         validate("GMM++", y_gmm, exprOperands.at("yRef"));
+        break;
+      }
+      case plus3: {
+        int rows=exprOperands.at("ARef").getDimension(0);
+        int cols=exprOperands.at("ARef").getDimension(1);
+        GmmDynSparse Agmm(rows,cols);
+        GmmDynSparse Bgmm(rows,cols);
+        GmmDynSparse Cgmm(rows,cols);
+        GmmDynSparse Dgmm(rows,cols);
+
+        tacoToGMM(exprOperands.at("B"),Bgmm);
+        tacoToGMM(exprOperands.at("C"),Cgmm);
+        tacoToGMM(exprOperands.at("D"),Dgmm);
+
+        TACO_BENCH(Agmm=Bgmm;gmm::add(Cgmm,Agmm);gmm::add(Dgmm,Agmm);,"GMM",repeat,timevalue,true);
+
+        Tensor<double> A_gmm({rows,cols}, CSC);
+        GMMTotaco(Agmm,A_gmm);
+
+        validate("GMM", A_gmm, exprOperands.at("ARef"));
         break;
       }
       default:
